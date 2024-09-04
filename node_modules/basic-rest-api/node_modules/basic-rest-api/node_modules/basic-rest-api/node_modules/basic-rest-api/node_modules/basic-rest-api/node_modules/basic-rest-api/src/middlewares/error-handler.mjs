f8bc8987b06539acb0178298a3252e93b12c08d6
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-09-04 16:54:21 -0300

    rompi todo

--- node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/src/middlewares/error-handler.mjs
+++
@@ -1,4 +0,0 @@
-export default (req, res, next) => {
-  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
-  next();
-};
--- node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/node_modules/basic-rest-api/src/middlewares/request-logger.mjs
+++
@@ -1,4 +0,0 @@
-export const requestLogger = (req, res, next) => {
-  console.log(`👉 [${new Date().toLocaleString()}] ${req.method} ${req.url}`);
-  next();
-};
