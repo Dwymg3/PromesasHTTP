85f47dbdc11c7aa0b1da39dc16b18cd5a622b7b0
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-09-04 17:00:09 -0300

    1ra v
    1ra version para levantar el localhoost

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import errorHandler from "./middlewares/error-handler.mjs";
 import { requestLogger } from "./middlewares/request-logger.mjs";
 import tasksRouter from "./routes/tasks.mjs";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,13 @@ app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(cors());
 app.use(errorHandler);
+
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+})
+
 
 app.use("/api", tasksRouter);
 
